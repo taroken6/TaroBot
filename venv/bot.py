a3ce993d5bb32d776532de145836c61a824d9dec
taroken6 <audiemendaros@yahoo.com>
2020-07-15 13:48:19 -0700

    Placed all general commands into own class. 'Connected' function moved from MusicCog to GeneralCog

--- venv/bot.py
+++ venv/bot.py
@@ -47,76 +47,84 @@ bot.remove_command('help')
 ##############################     GENERAL COMMANDS     ##############################
 
 # TODO: Create a cog class for general commands
+class GeneralCog(commands.Cog):
+    @bot.event
+    async def on_ready():
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game("Yeet"))
+        print('We have logged in as {0.user}'.format(bot))
 
-@bot.event
-async def on_ready():
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game("Yeet"))
-    print('We have logged in as {0.user}'.format(bot))
+    @commands.command(pass_context=True, aliases=['r'])
+    async def reply(self, ctx, *, question):
+        responses = ["Yes", "Maybe", "No"]
+        await ctx.send(f"question: {question}\nanswer: {random.choice(responses)}")
 
-@bot.command(pass_context=True, aliases=['r'])
-async def reply(ctx, *, question):
-    responses = ["Yes", "Maybe", "No"]
-    await ctx.send(f"question: {question}\nanswer: {random.choice(responses)}")
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'Pong! {round(bot.latency * 1000)} ms')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(bot.latency * 1000)} ms')
+    @commands.command(name="id")
+    async def getID(self, ctx):
+        await ctx.send(ctx.author.id)
 
-@bot.command(name="id")
-async def getID(ctx):
-    await ctx.send(ctx.author.id)
+    @commands.command(name='connect', aliases=['join'])
+    async def _connect(self, ctx):
+        bot_voice = ctx.voice_client
+        try:
+            user_channel = ctx.author.voice.channel
+        except:
+            return await ctx.send("You're currently not in a voice channel!")
 
-@bot.command(name='connect', aliases=['join'])
-async def _connect(ctx):
-    bot_voice = ctx.voice_client
-    try:
-        user_channel = ctx.author.voice.channel
-    except:
-        return await ctx.send("You're currently not in a voice channel!")
+        if not bot_voice:
+            await user_channel.connect()
+        elif bot_voice.channel != user_channel:
+            await bot_voice.disconnect()
+            await user_channel.connect()
 
-    if not bot_voice:
-        await user_channel.connect()
-    elif bot_voice.channel != user_channel:
-        await bot_voice.disconnect()
-        await user_channel.connect()
+    @commands.command(name='disconnect', aliases=['leave'])
+    async def _disconnect(self, ctx: discord.ext.commands.Context):
+        bot_voice = ctx.voice_client
 
-@bot.command(name='disconnect', aliases=['leave'])
-async def _disconnect(ctx: discord.ext.commands.Context):
-    bot_voice = ctx.voice_client
+        try:
+            user_channel = ctx.author.voice.channel
+        except:
+            return await ctx.send("Please join a voice channel")
 
-    try:
-        user_channel = ctx.author.voice.channel
-    except:
-        return await ctx.send("Please join a voice channel")
+        if user_channel != bot_voice.channel: return await ctx.send("We're not in the same voice channel")
 
-    if user_channel != bot_voice.channel: return await ctx.send("We're not in the same voice channel")
+        try:
+            await ctx.message.guild.voice_client.disconnect()
+        except:
+            return await ctx.send("I'm not in a server.")
 
-    try:
-        await ctx.message.guild.voice_client.disconnect()
-    except:
-        return await ctx.send("I'm not in a server.")
+    @commands.command(name="help")
+    async def _help(self, ctx):
+        embed = discord.Embed(title="Commands", color=discord.Color.light_grey())
 
-@bot.command(name="help")
-async def _help(ctx):
-    embed = discord.Embed(title="Commands", color=discord.Color.light_grey())
+        temp_general = bot.commands
+        general_text = ''
+        for command in temp_general:
+            general_text += f"{command}\n"
+        embed.add_field(name="General commands:",
+                        value= general_text,
+                        inline=True)
 
-    temp_general = bot.commands
-    general_text = ''
-    for command in temp_general:
-        general_text += f"{command}\n"
-    embed.add_field(name="General commands:",
-                    value= general_text,
-                    inline=True)
+        temp_music = MusicCog.get_commands(bot.get_cog('MusicCog'))
+        music_text = ''
+        for command in temp_music:
+            music_text += f"{command}\n"
+        embed.add_field(name="Music commands:",
+                        value=music_text,
+                        inline=True)
 
-    temp_music = MusicCog.get_commands(bot.get_cog('MusicCog'))
-    music_text = ''
-    for command in temp_music:
-        music_text += f"{command}\n"
-    embed.add_field(name="Music commands:",
-                    value=music_text,
-                    inline=True)
+        await ctx.send(embed=embed)
 
-    await ctx.send(embed=embed)
+    @commands.command(name='connected')
+    async def _connected(self, ctx):
+        voice = get(bot.voice_clients, guild=ctx.guild)
+        if voice is None:
+            await ctx.send("Not connected")
+        elif voice.is_connected():
+            await ctx.send("Connected...")
 
 ##############################     MUSIC PLAYER / YTDL    ##############################
 
@@ -346,14 +354,6 @@ class MusicCog(commands.Cog):
             bot_voice.source.volume = vol / 100
             await ctx.send(f"Volume set to {vol}%")
 
-    @commands.command(name='connected')
-    async def _connected(self, ctx):
-        voice = get(bot.voice_clients, guild=ctx.guild)
-        if voice is None:
-            await ctx.send("Not connected")
-        elif voice.is_connected():
-            await ctx.send("Connected...")
-
 ##############################     MEME COMMANDS    ##############################
 
 @bot.command()
@@ -435,5 +435,6 @@ class SoundCog(commands.Cog):
 def setup(bot):
     bot.add_cog(MusicCog(bot))
     bot.add_cog(SoundCog(bot))
+    bot.add_cog(GeneralCog(bot))
 setup(bot)
 bot.run(TOKEN)
