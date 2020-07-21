1b0adb91bb83e43199d4c2c3f8ebf32a7efe7337
taroken6 <audiemendaros@yahoo.com>
2020-07-21 14:19:02 -0700

    SoundCog: Error handling. Users prompted when overwriting an existing sound file. Better help description.

--- venv/cogs/SoundCog.py
+++ venv/cogs/SoundCog.py
@@ -22,11 +22,10 @@ class SoundCog(commands.Cog):
         self.json_file = "soundlist.txt"
         self.soundlist = {}
 
-        self.deserialize()
-        for file in os.listdir(sound_folder):
+        self.deserialize()  # Get existing sounds in JSON file
+        for file in os.listdir(sound_folder):  # Get other mp3 files in folder that aren't in JSON file
             file_name = file.split('.mp3')[0]
-            if not self.soundlist[file_name]:
-                print("Found unknown file in sound dir. Creating new sound with default params")
+            if file_name not in self.soundlist.keys():
                 self.soundlist[file_name] = Sound(
                     file_name,
                     f"{sound_folder}{file}",
@@ -39,7 +38,9 @@ class SoundCog(commands.Cog):
         for i in range(1, pages + 1):
             embeds.append(discord.Embed(
                 title=f"Sounds commands, Page {i} of {pages}",
-                description="t!s [sound] [volume (0 - 200)]",
+                description="t!s [sound name] [volume (0 - 200)]\n"
+                            "t!s dl [myinstants.com URL] [name] [description]\n"
+                            "example: t!s dl https://www.myinstants.com/instant/crickets/ cricket \"cricket noises\"",
                 color=discord.Colour.teal()
             ))
         for idx, (key, value) in enumerate(self.soundlist.items()):
@@ -48,13 +49,14 @@ class SoundCog(commands.Cog):
                                    value=self.soundlist[key].desc,
                                    inline=True)
         message = await ctx.send(embed=embeds[0])
+
         left_arrow = '⬅'
         right_arrow = '➡'
         await message.add_reaction(left_arrow)
         await message.add_reaction(right_arrow)
-
         page = 0
         reaction = ''
+
         message_timeout = time.time() + 60
         while time.time() < message_timeout:
             if reaction and reaction.member.id != self.bot.user.id:
@@ -71,10 +73,31 @@ class SoundCog(commands.Cog):
 
     async def _download(self, ctx, url, name, desc):
         # TODO: May need better error handling
+        if 'myinstants.com/instant/' not in url:
+            return await ctx.send("URL must contain 'myinstants.com/instant/'")
+        if name in self.soundlist.keys():
+            msg = await ctx.send(f"The sound '{name}' already exists! Would you like to overwrite?")
+            check = '✅'
+            x = '❌'
+            await msg.add_reaction(check)
+            await msg.add_reaction(x)
+
+            reaction = ''
+            message_timeout = time.time() + 60
+            while time.time() < message_timeout:
+                if reaction and reaction.member.id != self.bot.user.id:
+                    if reaction.emoji.name == check:
+                        break
+                    if reaction.emoji.name == x:
+                        return await ctx.send("Download cancelled.")
+                try:
+                    reaction = await self.bot.wait_for('raw_reaction_add', timeout=60.0)
+                except:
+                    return await ctx.send("Timed out. Sound not downloaded.")
+
         page = requests.get(url)
         soup = BeautifulSoup(page.text, "html.parser")
         dl_url = ''
-
         for link in soup.find_all('a', {'class': "instant-page-extra-button"}):
             search = re.search('/media.*\.mp3', link.get('href'))
             if search:
@@ -90,6 +113,7 @@ class SoundCog(commands.Cog):
             f.write(mp3_file.content)
         self.serialize(sound)
         self.deserialize()
+        await ctx.send("Download complete!")
 
     def serialize(self, sound):
         name, file, desc, url = sound.name, sound.file, sound.desc, sound.url
@@ -99,6 +123,7 @@ class SoundCog(commands.Cog):
                   'desc': desc,
                   'url': url
                   }}
+
         for s in self.soundlist.keys():
             sound = self.soundlist[s]
             data[sound.name] = {
@@ -107,6 +132,7 @@ class SoundCog(commands.Cog):
                 'desc': sound.desc,
                 'url': sound.url
             }
+
         with open(self.json_file, 'w') as sl:
             json.dump(data, sl, indent=2)
             sl.close()
@@ -121,7 +147,6 @@ class SoundCog(commands.Cog):
             for key, value in payload.items():
 
                 if not key in self.soundlist:
-                    print(f"DEBUG: {key} not found. Creating new Sound object")
                     self.soundlist[key] = Sound(value['name'], value['file'], value['desc'], value['url'])
 
     @commands.command(name='sound', aliases=['s'])
@@ -142,7 +167,7 @@ class SoundCog(commands.Cog):
             except:
                 return await ctx.send("Bad input: 't!s dl [url] [name] [desc]'")
             await self._download(ctx, url, name, desc)
-        else:  ###### Play sound ######
+        elif sound in self.soundlist.keys():  ###### Play sound ######
             default_vol = 0.20
             try:
                 vol = (float)(args[1]) / 100
@@ -157,5 +182,7 @@ class SoundCog(commands.Cog):
                 else:
                     return await ctx.send("Either me or you are not in a voice channel")
             voice.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(self.soundlist[sound].file), vol), after=None)
+        else:
+            print("Error in SoundCog. Invalid Input.")
 
 # TODO: Allow it so that users can create their own sound command 3rd
