51793b7d739bbe8515fc3e9ad3e8de07e6eb758c
taroken6 <audiemendaros@yahoo.com>
2020-07-23 14:34:35 -0700

    Added a 'delete' sound function to SoundCog

--- venv/cogs/SoundCog.py
+++ venv/cogs/SoundCog.py
@@ -17,6 +17,7 @@ import json
 sound_folder = os.getcwd() + '\sounds\\'
 help_param = ['help', 'h']
 dl_param = ['download', 'dl']
+del_param = ['delete', 'del', 'd']
 reserved_names = help_param + dl_param
 
 class SoundCog(commands.Cog):
@@ -35,6 +36,9 @@ class SoundCog(commands.Cog):
                     'Description missing', 'URL missing')
 
     def serialize(self):
+        '''
+        Writes all Sound objects in dict to JSON
+        '''
         data = {}
         for s in self.soundlist.keys():
             sound = self.soundlist[s]
@@ -51,13 +55,11 @@ class SoundCog(commands.Cog):
 
     def deserialize(self):
         '''
-        Unloads serialized dictionary of Sound objects to soundlist
+        Unloads JSON to sound dict
         '''
-        # TODO: What if the mp3 file doesn't exist?
         with open(self.json_file) as f:
             payload = json.load(f)
             for key, value in payload.items():
-
                 if not key in self.soundlist:
                     self.soundlist[key] = Sound(value['name'], value['file'], value['desc'], value['url'])
 
@@ -160,6 +162,15 @@ class SoundCog(commands.Cog):
                 return await ctx.send("Either me or you are not in a voice channel")
         voice.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(self.soundlist[sound].file), vol), after=None)
 
+    async def _delete(self, ctx, name):
+        '''
+        Remove sound from dict and then resets it
+        '''
+        self.soundlist.pop(name, None)
+        self.serialize()
+        self.deserialize()
+        await ctx.send(f"'{name}' deleted successfully!")
+
     @commands.command(name='sound', aliases=['s'])
     async def _sound(self, ctx, *args):
         try:
@@ -169,17 +180,20 @@ class SoundCog(commands.Cog):
 
         if not sound or sound in help_param:  ###### Help request ######
             return await self._help(ctx)
-        elif sound in dl_param:
+        if sound in del_param:
+            try: name = args[1]
+            except: return await ctx.send("Please specify a sound to delete")
+            return await self._delete(ctx, name)
+        if sound in dl_param:
             try:
                 url, name, desc = args[1], args[2], args[3]
             except:
                 return await ctx.send("Bad input: 't!s dl [url] [name] [desc]'")
             await self._download(ctx, url, name, desc)
-        elif sound in self.soundlist.keys():  ###### Play sound ######
+        if sound in self.soundlist.keys():  ###### Play sound ######
             try: vol = args[1]
             except: vol = 20
             return await self._play(ctx, sound, vol)
         else:
-            print("Error in SoundCog. Invalid Input.")
+            return await ctx.send("Sound does not exist")
 
-# TODO: Allow it so that users can create their own sound command 3rd
